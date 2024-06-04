6c3fdfb00df41074d9ccb001a25e1b270b38a54e
bdg <dongb94@gmail.com>
2024-06-04 15:11:13 +0900

    Feat : add rudp and enet

---
+++ FusionVersusMVS/Assets/MVS/Realtime/MVSSocketRudp.cs
@@ -0,0 +1,67 @@
+﻿
+
+using System.Threading;
+using UnityEngine.Scripting;
+using ENet;
+
+namespace MVS.Realtime
+{
+    
+    public class MVSSocketRudp : RealtimeSocketConnection
+    {
+        private Host _client;
+        private Peer _server;
+        
+        private readonly object syncer = new object();
+        
+        [Preserve]
+        public MVSSocketRudp(PeerBase peerBase) : base(peerBase)
+        {
+            Library.Initialize();
+
+            _client = new Host();
+            
+        }
+
+        ~MVSSocketRudp()
+        {
+            Library.Deinitialize();
+        }
+
+        public override bool Connect()
+        {
+            lock (syncer)
+            {
+                if (!base.Connect())
+                    return false;
+                State = RealtimeSocketState.Connecting;
+            }
+            // new Thread(DnsAndConnect)
+            // {
+            //     IsBackground = true
+            // }.Start();
+            return true;
+        }
+
+        internal void ConnectToServer()
+        {
+            
+        }
+
+        public override bool Disconnect()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool Send(byte[] data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool Receive(byte[] data)
+        {
+            throw new System.NotImplementedException();
+        }
+        
+    }
+}
